31849a222892b518cfb6c6e6e3d3e9dd6a229220
Sangtavana <kelly.manichanh@gompersstudent.org>
2017-04-17 14:56:28 -0700

    Add files via upload

---
+++ lly.java
@@ -0,0 +1,22 @@
+package robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class lly {
+	public static void main(String[] args) {
+		Robot sang= new Robot();
+		sang.setSpeed(10);
+		sang.penDown();
+		sang.move(250);
+		sang.turn(180);
+		sang.move(100);
+		sang.turn(225);
+		sang.move(100);
+		sang.turn(180);
+		sang.move(100);
+		sang.turn(250);
+		sang.move(250);
+		
+	}
+
+}
