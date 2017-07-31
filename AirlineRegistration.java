f59270165860b05a45454e2e07adef0b5a79888d
Sangtavana <kelly.manichanh@gompersstudent.org>
2017-07-31 14:55:09 -0700

    Add files via upload

---
+++ AirlineRegistration.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class AirlineRegistration {
+	public static void main(String[] args){
+		
+		String Airline= JOptionPane.showInputDialog(null,"First Name:");
+		String Airline2= JOptionPane.showInputDialog(null,"Last Name:");
+		
+		
+		
+		
+}
+}
